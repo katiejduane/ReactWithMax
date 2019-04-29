359512a676bd36d0a7f4f22463df8f64e7c6fb17
Katie Duane <katiejduane@gmail.com>
2019-04-29 00:08:47 -0400

    side drawer CSS finished

--- 8-basicBurgerApp/src/components/Navigation/Toolbar/Toolbar.js
+++ 8-basicBurgerApp/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './Toolbar.css'
 import Logo from '../../Logo/Logo';
 import NavItems from '../NavItems/NavItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
     <header className="Toolbar">
-        <div>MENU</div>
+        <DrawerToggle clicked={props.drawerToggleClicked}/>
         <Logo height="80%"/>
         <nav className ="DesktopOnly">
             <NavItems />
