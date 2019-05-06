25a1f3d8faa0e8432f0bed42ec4a999c48ab6149
Katie Duane <katiejduane@gmail.com>
2019-05-06 17:05:24 -0400

    implementing router

--- 8-basicBurgerApp/src/containers/BurgerBuilder/BurgerBuilder.js
+++ 8-basicBurgerApp/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -90,38 +90,38 @@ class BurgerBuilder extends Component{
 
     purchaseContinueHandler = () => {
         // alert('You continue!');
-        this.setState({loading: true});
-        const order = {
-          ingredients: this.state.ingredients,
-          price: this.state.totalPrice,
-          customer: {
-              name: 'Max Schwarzmuller',
-              address: {
-                  street: 'Teststreet 1',
-                  zipCode: '12345',
-                  country: 'Germany'
-              },
-              email: 'test@test.com'
-          },
-          deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false});
-            })
-            .catch(error => {
-                this.setState({loading: false, purchasing: false});
-            });
-            
-        // const queryParams = [];
-        // for(let i in this.state.ingredients){
-        //     queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
+        // this.setState({loading: true});
+        // const order = {
+        //   ingredients: this.state.ingredients,
+        //   price: this.state.totalPrice,
+        //   customer: {
+        //       name: 'Max Schwarzmuller',
+        //       address: {
+        //           street: 'Teststreet 1',
+        //           zipCode: '12345',
+        //           country: 'Germany'
+        //       },
+        //       email: 'test@test.com'
+        //   },
+        //   deliveryMethod: 'fastest'
         // }
-        // const queryString = queryParams.join('&')
-        // this.props.history.push({
-        //     pathname: '/checkout',
-        //     search: '?' + queryString
-        // })
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({loading: false, purchasing: false});
+        //     })
+        //     .catch(error => {
+        //         this.setState({loading: false, purchasing: false});
+        //     });
+            
+        const queryParams = [];
+        for(let i in this.state.ingredients){
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
+        }
+        const queryString = queryParams.join('&')
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        })
 
     }
  
