124c7b3a3c18e2600aea6cfc64ddad477c164c43
Katie Duane <katiejduane@gmail.com>
2019-05-28 21:04:48 -0400

    moving onto redux advanced

--- 14-redux/redux--03-react-redux/src/store/reducers/counter.js
+++ 14-redux/redux--03-react-redux/src/store/reducers/counter.js
@@ -1,8 +1,7 @@
-import * as actionTypes from './actions';
+import * as actionTypes from '../actions';
 
 const initialState = {
-    counter: 0,
-    results: []
+    counter: 0
 }
 
 const reducer = (state = initialState, action) => {
@@ -17,7 +16,7 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 counter: state.counter - 1
             }
-        case actionTypes.ADD: 
+        case actionTypes.ADD:
             return {
                 ...state,
                 counter: state.counter + action.payload
@@ -26,17 +25,6 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 counter: state.counter - action.payload
-            }
-        case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value: state.counter})
-            }
-        case actionTypes.DELETE_RESULT:
-            const updatedArray = state.results.filter(result => result.id !== action.elemId)
-            return {
-                ...state,
-                results: updatedArray
             }
     }
     return state;
